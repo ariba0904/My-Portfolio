93a1016acd4abfdb3e7158e0aad54091e21ea0f1
Jannatul Maowa Ariba <175639245+ariba0904@users.noreply.github.com>
2026-01-23 19:07:22 +0600

    Update script.js
    added scroll reveal

--- script.js
+++ script.js
@@ -43,8 +43,25 @@ document.addEventListener('DOMContentLoaded', function () {
       }, 500);
     });
   }
-
 });
+
+// ---------- SCROLL REVEAL ----------
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+    if (cardTop < windowHeight - 100) {
+      card.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+revealOnScroll();
+
 
 
 
